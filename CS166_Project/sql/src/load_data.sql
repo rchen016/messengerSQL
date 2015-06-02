a1b7ef4feea52724c6cd99da1d609fc38538fd96
Ricky Chen <rchen016@well.cs.ucr.edu>
2015-06-01 22:12:03 -0700

    loaded csv files

---
+++ CS166_Project/sql/src/load_data.sql
@@ -0,0 +1,20 @@
+COPY MESSAGE(msgId,senderId,receiverId,contents,sendTime,deleteStatus,status) 
+FROM '/tmp/rchen016/CS166_Project/data/Message.csv' 
+WITH DELIMITER ',' CSV HEADER;
+
+
+COPY EDUCATIONAL_DETAILS(userId,instituitionName,major,degree,startdate,enddate) 
+FROM '/tmp/rchen016/CS166_Project/data/Edu_Det.csv' 
+WITH DELIMITER ',' CSV HEADER;
+
+COPY CONNECTION_USR(userId,connectionId,status) 
+FROM '/tmp/rchen016/CS166_Project/data/Connection.csv' 
+WITH DELIMITER ',' CSV HEADER;
+
+COPY USR(userId,password,email,name,dateofBirth) 
+FROM '/tmp/rchen016/CS166_Project/data/USR.csv' 
+WITH DELIMITER ',' CSV HEADER;
+
+COPY WORK_EXPR(userId,company,role,location,startDate,endDate)
+FROM '/tmp/rchen016/CS166_Project/data/Work_Ex.csv' 
+WITH DELIMITER ',' CSV HEADER;
